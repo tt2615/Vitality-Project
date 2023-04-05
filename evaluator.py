fix training loss computation bug

evaluator.py
@@ -1,12 +1,7 @@
 import torch
-from torch.utils.data import DataLoader
 
 class R2_SCORE:
-
-    def __init__(self):
-        pass
-
-    def compute(self, y, y_pred):
+    def __call__(self, y, y_pred):
         # Compute the target mean for each task
         y_mean = torch.mean(y, dim=0) #[1, task]
 

main.py
@@ -95,19 +95,20 @@ if args.model_path:
 else: 
     print("-"*10 + "Start training " + "-"*10)
 
-    t_epoch = trange(args.epoch, leave=False)
-    epoch_loss = 0
-
+    # paramters for early stop
     best_loss = -np.inf
     min_delta=0.001
     counter = 0
     patience = 10
     stop_training = False
 
+    t_epoch = trange(args.epoch, leave=False)
+    epoch_loss = 0
     for epoch in t_epoch:
         logging.debug(f"EPOCH {epoch}\n")
         t_epoch.set_description(f"Epoch {epoch} - avg loss: {epoch_loss/len(train_dataloader)}")
         t_epoch.refresh()
+        epoch_loss = 0 #reset epoch loss for current epoch training
         
         t_batch = tqdm(train_dataloader, leave=False)
         batch_loss = 0
@@ -125,8 +126,11 @@ else:
             pred = model(x)
             # print(x.shape,y.shape, pred.shape) [256, 9] [256, 3] [256, 3] 
             
+            print(batch_loss)
             batch_loss = model.compute_loss(pred, y)
+            print(batch_loss)
             epoch_loss += batch_loss
+            
 
             # backpropagation
             optimizer.zero_grad()
@@ -134,15 +138,13 @@ else:
             optimizer.step()
             # torch.nn.utils.clip_grad_norm(parameters=model.parameters(), max_norm=10, norm_type=2.0)
 
-            # early stop
-
             time.sleep(0.01)
 
         # eavluate on test data
         train_loss = epoch_loss/len(train_dataloader)
         valid_loss, metrics = model.eval(valid_dataloader, device)
         logging.debug(f"train loss: {train_loss}\n")
-        logging.debug(f"test loss: {valid_loss}\n")
+        logging.debug(f"valid loss: {valid_loss}\n")
         logging.debug(f"metrics performance: {metrics}\n")
         logging.debug('-'*10+'\n')
 

models.py
@@ -19,13 +19,6 @@ class LR(nn.Module):
         self.evaluators = [R2_SCORE()]
 
     def forward(self, x):
-        """
-        > The function `forward` takes in an input `x` and returns a prediction `preds` based on the weights
-        `self.W` and bias `self.b`
-        
-        :param x: the input data
-        :return: The prediction of the model.
-        """
         out = self.linear(x)
         return out
     
@@ -50,6 +43,6 @@ class LR(nn.Module):
                 eval_loss = self.compute_loss(pred, y)
 
                 for e in self.evaluators:
-                    metrics_vals[type(e).__name__] += e.compute(y, pred) #[1, task]
+                    metrics_vals[type(e).__name__] += e(y, pred) #[1, task]
 
             return eval_loss, metrics_vals